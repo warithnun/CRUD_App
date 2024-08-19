6304039b1bdcf613925d178b3bc1825b97736c63
wrt <warithnun.ph@rmuti.ac.th>
2024-08-19 14:41:21 +0700

    update

--- Controllers/product.js
+++ Controllers/product.js
@@ -14,3 +14,39 @@ exports.list = async(req,res)=>{
 
     }
 }
+exports.create = async(req,res)=>{
+    try{
+        //code
+        res.send('Hello Create')
+
+    } catch(err){
+        // error
+        console.log(err)
+        res.status(500).send('Server Error')
+
+    }
+}
+exports.update = async(req,res)=>{
+    try{
+        //code
+        res.send('Hello Update')
+
+    } catch(err){
+        // error
+        console.log(err)
+        res.status(500).send('Server Error')
+
+    }
+}
+exports.remove = async(req,res)=>{
+    try{
+        //code
+        res.send('Hello Delete')
+
+    } catch(err){
+        // error
+        console.log(err)
+        res.status(500).send('Server Error')
+
+    }
+}
--- Routes/product.js
+++ Routes/product.js
@@ -1,20 +1,24 @@
 const express = require('express')
 const router = express.Router()
-const { read } = require('../Controllers/product')
-const { list } = require('../Controllers/product')
+const { read,list,create,update,remove } = require('../Controllers/product')
+
 
 router.get('/product',list)
-
 router.get('/product/:id',read)
+router.post('/product',create)
+router.put('/product/:id',update)
+router.delete('/product/:id',remove)
 
-router.post('/product',(req,res)=>{
-    res.send('Hello Post Endpoint')
-})
-router.put('/product/:id',(req,res)=>{
-    res.send('Hello put Endpoint')
-})
-router.delete('/product/:id',(req,res)=>{
-    res.json({name:'wrt',id:555})
-})
+
+
+// router.post('/product',(req,res)=>{
+//     res.send('Hello Post Endpoint')
+// })
+// router.put('/product/:id',(req,res)=>{
+//     res.send('Hello put Endpoint')
+// })
+// router.delete('/product/:id',(req,res)=>{
+//     res.json({name:'wrt',id:555})
+// })
 
 module.exports =router
